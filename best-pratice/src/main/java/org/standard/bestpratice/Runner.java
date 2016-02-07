b16b7ac71977ffe5bc86e959c4eaedf6c7e7d501
wallace <wallace@computhand>
2016-02-06 23:14:57 -0500

    montreal Light

--- best-pratice/src/main/java/org/standard/bestpratice/Runner.java
+++ best-pratice/src/main/java/org/standard/bestpratice/Runner.java
@@ -1,10 +1,20 @@
 package org.standard.bestpratice;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import org.slf4j.Logger;
 
@@ -21,19 +31,99 @@ public class Runner {
 
         LOG.debug("Run fun stuff to be sure we are able to comput in Java");
 
-        String phoneIdI = "0P2";
-        String phoneIdJ = "0P1";
+                JSONParser parser = new JSONParser();
+        
+        StringBuilder json = callService();
+        
+        Object trafficCamInfo;
+        try {
+            trafficCamInfo = parser.parse(json.toString());
+            JSONObject trafficCamInfoJSON = (JSONObject) trafficCamInfo;
+            
+            //LOG.debug(trafficCamInfoJSON.get("features").toString());
+            JSONArray features = (JSONArray)trafficCamInfoJSON.get("features");
+            
 
-
-
-        if (Integer.parseInt(phoneIdI.replaceAll("[\\D]", "")) > Integer.parseInt(phoneIdJ.replaceAll("[\\D]", ""))) {
-            LOG.debug("Integer " + Integer.parseInt(phoneIdI.replaceAll("[\\D]", "")));
+            //LOG.debug(features.get(1).toString());        
+            JSONObject feature1 = (JSONObject)features.get(1);
+            
+            LOG.debug(feature1.get("geometry").toString());
+            JSONObject geometry = (JSONObject)feature1.get("geometry");
+            
+            LOG.debug(geometry.get("coordinates").toString());
+            JSONArray coordinates = (JSONArray)geometry.get("coordinates");
+            
+            LOG.debug("Lantitude : " + coordinates.get(0).toString() + ", Longitude : " + coordinates.get(1).toString());
+            
+        } catch (ParseException ex) {
+            LOG.error("json parsing want wrong", ex);
         }
 
 
         LOG.debug("We did it right, we can now use it in our code ");
     }
+    
+    /**
+     * Call the camera information service for Montreal open data.
+     *
+     * @return StringBuilder the JSON service output.
+     */
+    public static StringBuilder callService() throws RuntimeException {
 
+        //TODO add to properties file
+        String camInfoURL = "http://ville.montreal.qc.ca/circulation/sites/ville.montreal.qc.ca.circulation/files/cameras-de-circulation.json";
+
+        StringBuilder output = null;
+
+        try {
+
+            URL url = new URL(camInfoURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            output = new StringBuilder();
+            String out;
+            LOG.debug("Output from Server .... \n");
+            while ((out = br.readLine()) != null) {
+                output.append(out);
+                LOG.debug(out);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException ex) {
+
+            LOG.error("the url to call is not valid : " + camInfoURL, ex);
+
+        } catch (IOException ex) {
+
+            LOG.error("json reading want wrong", ex);
+
+        }
+
+        return output;
+
+    }
+    
+    public void convertStringToInteger(){
+        
+        String phoneIdI = "0P2";
+        String phoneIdJ = "0P1";
+
+        if (Integer.parseInt(phoneIdI.replaceAll("[\\D]", "")) > Integer.parseInt(phoneIdJ.replaceAll("[\\D]", ""))) {
+            LOG.debug("Integer " + Integer.parseInt(phoneIdI.replaceAll("[\\D]", "")));
+        }
+    }
+    
     public void getNextId() {
 
         int id = 1;
